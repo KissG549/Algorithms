5f2ec3edc9148c0bf2e88491992572f9b700c5c7
KissG549 <57135398+KissG549@users.noreply.github.com>
2020-06-03 21:07:50 +0200

    Update hackerrank_virtual_functions.cpp

--- hackerrank_virtual_functions.cpp
+++ hackerrank_virtual_functions.cpp
@@ -2,7 +2,12 @@
   Source: https://www.hackerrank.com/challenges/virtual-functions/problem
 */
 
-
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
 class Person
 {
@@ -65,3 +70,30 @@ class Student:public Person
 
 unsigned Professor::id = 0;
 unsigned Student::id = 0;
+
+int main(){
+
+    int n, val;
+    cin>>n; //The number of objects that is going to be created.
+    Person *per[n];
+
+    for(int i = 0;i < n;i++){
+
+        cin>>val;
+        if(val == 1){
+            // If val is 1 current object is of type Professor
+            per[i] = new Professor;
+
+        }
+        else per[i] = new Student; // Else the current object is of type Student
+
+        per[i]->getdata(); // Get the data from the user.
+
+    }
+
+    for(int i=0;i<n;i++)
+        per[i]->putdata(); // Print the required output for each object.
+
+    return 0;
+
+}
