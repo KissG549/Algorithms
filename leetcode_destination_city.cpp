9211da5a483336eefa739b74d47b6733f6fbf691
KissG549 <57135398+KissG549@users.noreply.github.com>
2020-08-31 14:19:32 +0200

    Update leetcode_destination_city.cpp

--- leetcode_destination_city.cpp
+++ leetcode_destination_city.cpp
@@ -1,4 +1,7 @@
 /*
+
+https://leetcode.com/problems/destination-city/
+
 1436. Destination City
 
 You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi. Return the destination city, that is, the city without any path outgoing to another city.
