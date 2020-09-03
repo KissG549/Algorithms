1d271169b91d8ee855234010dc2cadedd3b060f7
KissG549 <57135398+KissG549@users.noreply.github.com>
2020-09-03 09:07:02 +0200

    Create fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int fib(int num)
+{
+    if(num == 0 || num == 1)
+    {
+        return num;
+    }
+
+    return fib(num-1) + fib(num -2);    
+}
+
+int main()
+{
+    for( int i = 0; i < 10; i++ )
+    {
+       cout << fib(i) << endl;
+    }
+}
