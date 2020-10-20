3064db7c3e8995354c5be70bbb9a2e1cf636ca8d
GK <57135398+KissG549@users.noreply.github.com>
2020-10-20 11:27:00 +0200

    Create leetcode_remove_duplicates_from_sorted_list_ii.cpp
    Given a sorted linked list, delete all duplicates such that each element appear only once.

---
+++ leetcode_remove_duplicates_from_sorted_list_ii.cpp
@@ -0,0 +1,97 @@
+/*
+    source: 
+      https://leetcode.com/problems/remove-duplicates-from-sorted-list
+    
+    Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+    Example 1:
+
+    Input: 1->1->2
+    Output: 1->2
+
+    Example 2:
+
+    Input: 1->1->2->3->3
+    Output: 1->2->3
+
+
+    
+    result:
+      Runtime: 16 ms, faster than 53.27% of C++ online submissions for Remove Duplicates from Sorted List.
+      Memory Usage: 12 MB, less than 75.29% of C++ online submissions for Remove Duplicates from Sorted List.
+      
+      
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    
+     ListNode* removeNode( ListNode *pPrevNode, ListNode* pNode)
+    {
+        /*
+            head->next points to the second node
+            second->next points to the third node
+            
+            to remove second
+            
+            head->next must point to the third node
+            then remove the second node
+        */
+        
+        if( pPrevNode == nullptr || pNode == nullptr )
+        {
+            return nullptr;
+        }
+        
+        pPrevNode->next = pNode->next;
+        
+        delete pNode;
+        
+        return pPrevNode->next;
+    }
+    
+    
+    ListNode* deleteDuplicates(ListNode* pHead) {
+        
+        // empty list
+        if( pHead == nullptr )
+        {
+            return nullptr;
+        }
+        
+        // one size list
+        if( pHead->next == nullptr )
+        {
+            return pHead;
+        }
+        
+        ListNode *firtNode = pHead;
+        ListNode *secondNode = pHead->next;
+        
+            
+        while( secondNode != nullptr )
+        {
+            if( firtNode->val == secondNode->val )
+            {
+                secondNode = removeNode( firtNode, secondNode );
+            }
+            else
+            {
+                firtNode = secondNode;
+                secondNode = secondNode->next;
+            }
+            
+        }
+        return pHead;
+    }
+};
